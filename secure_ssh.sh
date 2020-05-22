ce37d5db5e777031166b9f63b6a7245526642780
shawn226 <35102149+shawn226@users.noreply.github.com>
2020-05-22 11:50:54 +0200

    test

--- secure_ssh.sh
+++ secure_ssh.sh
@@ -2,11 +2,22 @@
 
 
 get_passwd(){
-	read -p "Voulez-vous un mot de passe ? (n/y) : " answer_pass
+	read -p "Voulez-vous un mot de passe ? (y/n) : " answer_pass
 	
-	if [[ $answer = "y" ]]
+	if [[ $answer_pass = "y" ]]
 	then
-		echo $answer
+		local error=0
+		while [[ $error = 0 ]]
+		do
+			read -p "Entrez un mot de passe : " password
+			read -p "Confirmez le mot de passe : " confirmed_pswd
+			
+			if [[ $password != $confirmed_pswd ]]
+			then
+				echo "Erreur les mots de passes ne correspondent pas !"
+			else
+				error=1
+		done	
 	fi
 }
 
