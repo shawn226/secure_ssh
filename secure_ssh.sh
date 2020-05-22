2b01589e55480bc5027def9181aed928687e50b0
shawn226 <35102149+shawn226@users.noreply.github.com>
2020-05-22 11:23:38 +0200

    test

--- secure_ssh.sh
+++ secure_ssh.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-(echo ""; echo ""; echo ""; echo "") | ssh-keygen
+
+get_passwd(){
+	read -p "Voulez-vous un mot de passe ? (n/y) : " answer_pass
+	
+	if [[ $answer = "y" ]]
+	then
+		echo $answer
+	fi
+}
+
+get_passwd
+
+# (echo ""; echo ""; echo ""; echo "") | ssh-keygen
