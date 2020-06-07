9d9299e32c932057c539959f566f7f8d9bc3aa7c
shawn226 <35102149+shawn226@users.noreply.github.com>
2020-06-07 16:23:15 +0200

    test

--- secure_ssh.sh
+++ secure_ssh.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 #Updating and installing ssh server
-echo "\e[96m[SecureApp] Initialization..."
+echo -e "\e[96m[SecureApp] Initialization..."
 sleep 1
 
 apt update && apt install openssh-server -y
 
 
 ###Config sshd_config###
-
-echo "\e[96m[SecureApp] Starting ssh configuration..."
+echo -e "\e[96m[SecureApp] Starting ssh configuration..."
 
 #Disable password authentification
 sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
@@ -26,7 +25,6 @@ sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_con
 echo 'AuthorizedKeysFile /home/backupclt/.ssh/authorized_keys' >> /etc/ssh/sshd_config
 
 #Disable logging with empty password
-
 sed -i 's/#PermitEmptyPassword no/PermitEmptyPassword no/' /etc/ssh/sshd_config
 
 #Set max authentification tries
@@ -41,16 +39,14 @@ iptables -A INPUT -i eth0 -p tcp -m state --dport 53120 --state NEW -m recent --
 
 service sshd restart
 
-echo "\e[92m[SecureApp] End of ssh configuration !"
+echo -e "\e[92m[SecureApp] End of ssh configuration !"
 
 
 ###Config Clamav anti-virus###
-
-echo "\e[96m[SecureApp] Starting Clamav configuration..."
+echo -e "\e[96m[SecureApp] Starting Clamav configuration..."
 
 
 # Install prerequisites and dependencies
-
 apt-get install build-essential -y # developer tools
 
 apt-get install openssl libssl-dev libcurl4-openssl-dev zlib1g-dev libpng-dev libxml2-dev libjson-c-dev libbz2-dev libpcre3-dev ncurses-dev -y # library dependencies
@@ -59,7 +55,6 @@ apt-get install valgrind check check-devel -y # unit testing dependencies
 
 
 # Download Clamav 
-
 cd
 
 wget https://www.clamav.net/downloads/production/clamav-0.102.3.tar.gz
@@ -83,11 +78,11 @@ make install
 
 
 # Setting up for the first time
-
-cp /etc/freshclam.conf.sample /etc/freshclam.conf # copy sample file to create a freshclam config
-
+cp /usr/local/etc/freshclam.conf.sample /usr/local/etc/freshclam.conf # copy sample file to create a freshclam config
 
 
+# Create the database directory
+mkdir /usr/local/share/clamav
 
 
 
